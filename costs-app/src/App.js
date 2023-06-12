dded6dbe3716a1c02620cdfe5f1ccd79fbd9e4cf
Xirley Anne Vale <renanvaled@gmail.com>
2023-06-11 22:19:08 -0300

    adicionado textos e routes

--- costs-app/src/App.js
+++ costs-app/src/App.js
@@ -1,9 +1,33 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import Container from './components/layout/Container';
 
 function App() {
+
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/newproject'>New Project</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/company'>Company</Link>
+      </div>
+      
+      <Container customClass="min-height">
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/company' element={<Company/>}/>
+            <Route path='/newproject' element={<NewProject/>}/>
+        </Routes>
+        </Container>
+    <p>Footer</p>
+    </Router>
   );
 }
 
